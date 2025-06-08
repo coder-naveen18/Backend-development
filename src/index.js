c9c89de8a0c6bad9ab9169b88b45d4ad435e3e97
Naveen sahu <codersahu18@gmail.com>
2025-06-08 14:38:55 +0530

    Database connected

---
+++ src/index.js
@@ -0,0 +1,41 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants"
+import connectDB from "./DB/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+// Way one to connect the Database within the index file
+
+/*
+import express from "express"
+const app = express()
+
+;( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log("Error:", error);
+            throw error
+            
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+
+        })
+    } catch (error) {
+        console.error("Error :", error)
+        throw error
+    }
+})()
+
+*/
