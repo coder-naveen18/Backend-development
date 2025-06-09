f5c78699b995b2906b860686363c094a7ba47fce
Naveen sahu <codersahu18@gmail.com>
2025-06-09 11:32:20 +0530

    custom Api

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import dotenv from "dotenv";
 // import {DB_NAME} from "./constants"
 import connectDB from "./DB/index.js";
 
+
 dotenv.config({
     path: './env'
 })
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection Failed !!!", error);
+    
+})
 
 
 // Way one to connect the Database within the index file
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler)=>{
+     (req,res,next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch((Error)=> next(Error))
+    }
+    
+}
+
+export {asyncHandler}
+
+
+
+
+// const asyncHandler = (fn)=> async (req, res, next)=>{
+//     try {
+//         await fu(req,res,next)
+        
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
