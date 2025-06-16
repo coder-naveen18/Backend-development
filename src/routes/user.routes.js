ee82ac1197515b9fa833538ec76427770980512a
Naveen sahu <codersahu18@gmail.com>
2025-06-16 15:11:44 +0530

    Added new routes for the user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser ,refreshAccessToken} from "../Controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser ,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvtar, updateUsercoverImage, getUserChannelProfile, getWatchHistory} from "../Controllers/user.controller.js";
 
 import {upload } from "../middlewares/multer.middleware.js"
 
@@ -26,6 +26,22 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh_token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-details").patch(verifyJWT, updateAccountDetails
+)
+
+router.route("/avtar").patch(verifyJWT, upload.single("avtar"), updateUserAvtar)
+
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUsercoverImage)
+
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 
